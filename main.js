5a253e3f9fa7abc1bf1d4f9015bc19ce3aef4cf5
assalikiin <fairuznajib04@gmail.com>
2026-06-14 13:58:13 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,34 @@
+const slides = document.querySelectorAll(".slide");
 
+let current = 0;
+
+setInterval(() => {
+slides[current].classList.remove("active");
+
+current++;
+
+if(current >= slides.length){
+current = 0;
+}
+
+slides[current].classList.add("active");
+
+},5000);
+
+function updateClock(){
+
+const now = new Date();
+
+document.getElementById("clock").innerHTML =
+now.toLocaleTimeString("id-ID");
+
+document.getElementById("date").innerHTML =
+new Intl.DateTimeFormat("id-ID",{
+dateStyle:"full"
+}).format(now);
+
+}
+
+setInterval(updateClock,1000);
+
+updateClock();
