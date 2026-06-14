51b72b6a3d3de85afa8a035ac27e6287943a707b
assalikiin <fairuznajib04@gmail.com>
2026-06-14 17:41:29 +0700

    Update main.js

--- main.js
+++ main.js
@@ -11,3 +11,21 @@ menuBtn.classList.toggle("active");
 menuItems.classList.toggle("show");
 
 });
+const observer = new IntersectionObserver((entries)=>{
+
+entries.forEach((entry)=>{
+
+if(entry.isIntersecting){
+
+entry.target.classList.add("show");
+
+}
+
+});
+
+},{
+threshold:0.15
+});
+
+document.querySelectorAll(".fade-up")
+.forEach((el)=>observer.observe(el));
